59182f5bf582906ec26ff51c7374874c178bc145
João D'Amorim <joao.damorim@gmail.com>
2018-01-28 00:11:19 -0300

    Test

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+class Hello:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def imprimir(nome):
+        print("Hello, %s!" % nome)
+
+Hello.imprimir("Joao")
